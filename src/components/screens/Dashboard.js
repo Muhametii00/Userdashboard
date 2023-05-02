9e50c0ec275ccd36d77bd0cc3793eceea4c28a9d
muhametii00 <konushevcimuhamet@gmail.com>
2023-05-02 10:51:29 +0200

    Style changes in topbar

--- src/components/screens/Dashboard.js
+++ src/components/screens/Dashboard.js
@@ -3,9 +3,18 @@ import WhiteCard from "../Cards.js/WhiteCard";
 
 export const Dashboard = () => {
   return (
-    <div>
-      <WhiteCard height="70%" size="100%">
-        <div>Dashboard</div>
+    <div style={{ marginTop: "10%" }}>
+      <WhiteCard height="90%" size="100%">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <p>Dashboard</p>
+          <p>Dashboard</p>
+        </div>
       </WhiteCard>
     </div>
   );
