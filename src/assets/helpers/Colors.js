285229c0e37ff6aaf60c5ffdb293f1fefa5d8b17
muhametii00 <konushevcimuhamet@gmail.com>
2023-05-03 09:55:44 +0200

    Added helpers for colors

---
+++ src/assets/helpers/Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: "#3C50E0",
+  lightblue: "#259AE6",
+  green: "#10B981",
+  black: "#212B36",
+  white: "#ffff",
+  lightgrey: "#8A99AF",
+  grey: "#64748B",
+  stroke: "#E2E8F0",
+};
